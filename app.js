6b8e5443e863f38ea5b0a5d8c71b4ac82a95440b
j-Sut <flowstategame@gmail.com>
2017-03-16 10:00:06 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//	1) Single State object: 
+var state = {
+	items: []
+}	
+
+//	2) functions that modify state: 
+alert("js is connected")
+
+// 3) Functions that render state:
+// The render functions should all take two arguments: 
+// the state object, and a DOM element to render into.
+
+$('header').css("background-color", "red");
+
+// 4) Event listeners
+
+
+
+
+
